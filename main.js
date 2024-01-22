2656ea3cf6107cdf461fdfe925794aa13ed19382
StartFromEnd <parkethan1210@gmail.com>
2024-01-22 05:23:34 +0000

    fix

--- main.js
+++ main.js
@@ -1393,7 +1393,7 @@ async function UploadImage(fileData){
         projectId: process.env.PROJECT_ID,
         credentials: {
           client_email: process.env.CLIENT_EMAIL,
-          private_key: process.env.PRIVATE_KEY,
+          private_key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
         },
     });
     
