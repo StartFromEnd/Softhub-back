cdc8d671bce4e6fdf329c7a407219e4c43d5d3cc
StartFromEnd <parkethan1210@gmail.com>
2024-01-25 07:03:59 +0000

    fix

--- main.js
+++ main.js
@@ -109,7 +109,7 @@ const Sign = async(email, name, resJson) => {
                 
                 resJson.msg = `환영합니다 ${result.length <= 0 ? name : result[0].user_nickname}님`;
                 
-                resJson.result = [session, (result.length <= 0 ? name : result[0].user_nickname)];
+                resJson.result = {sessionID: session, nickname: (result.length <= 0 ? name : result[0].user_nickname)};
                 
                 return resJson;
             }
@@ -122,7 +122,7 @@ const Sign = async(email, name, resJson) => {
 
         resJson.msg =
             '데이터를 확인하던 중 오류가 발생하였습니다. _SIGN_UP_Error: ' + `${stamp}`;
-        resJson.result = [error.message];
+        resJson.result = {error: error.message};
 
         conn.release();
         
